3ba0d5082ad46f51876cc213832278c67b7bc96e
Lennox <contact@lsco.dev>
2025-07-14 02:22:58 +0200

    bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter, Fira_Code } from 'next/font/google';
 import './globals.css';
-import { MetaData } from '@/config/Metadata';
 
 const inter = Inter({
   variable: '--font-sans',
